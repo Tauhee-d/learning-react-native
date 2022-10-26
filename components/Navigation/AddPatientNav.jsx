d4377192c631605004f6f59b8229f1d762ddb2bf
Tauhee-d <110287045+Tauhee-d@users.noreply.github.com>
2022-10-26 18:08:36 +0530

    changes made in nav

---
+++ components/Navigation/AddPatientNav.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+const AddPatientNav = () => {
+    return (
+        <View>
+            <Text>AddPatientNav</Text>
+        </View>
+    )
+}
+
+export default AddPatientNav
+
+const styles = StyleSheet.create({})
